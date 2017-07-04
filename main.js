1390d9aabc213cea75454f18f60c13fc5a068e80
zewillze <you_youzzw@sina.com>
2017-07-04 17:35:57 +0800

    add all

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+var app = require('app')
+var BrowserWindow = require('browser-window');
+var mainWindow = null;
+
+app.on('ready', function(){
+	mainWindow = new BrowserWindow({
+		height: 700,
+		resizable: false,
+		width: 368
+	});
+	mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+});
